31de4893e548e1c406f84229550845e6ae6f48f1
Quan Hua <quanhua92@gmail.com>
2023-03-31 16:07:43 +0700

    update docs

---
+++ docs/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"algorithms":[["impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"algorithms/data_structures/struct.LinkedList.html\" title=\"struct algorithms::data_structures::LinkedList\">LinkedList</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
